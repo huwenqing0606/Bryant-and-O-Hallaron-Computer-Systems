906d350c9de7cf8c6406ab5f628cecaccd2b78dc
huwenqing0606 <huwenqing.pku@gmail.com>
2023-10-02 21:16:09 -0500

    some examples from ch8

---
+++ 08_waitpid1.c
@@ -0,0 +1,14 @@
+#include "csapp.h"
+#define N 2
+
+int main(){
+    int status, i;
+    pid_t pid;
+
+    /* Parent creates N children */
+    for (i=0; i<N; i++) {
+        if ((pid=Fork())==0) /* child */
+            exit(100+i);
+    }
+}
+
